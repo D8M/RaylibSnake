1dbaf341a9f3cf3d0384a87643fcace4b2c6c1ec
D8M <davidmahon0000@gmail.com>
2025-07-05 15:31:34 +0100

    Added random method for food to appear randomly in the Food class

--- main.cpp
+++ main.cpp
@@ -14,11 +14,35 @@ int cellCount = 25;
 class Food {
 
 public:
-    Vector2 position = {5, 6};//Vector 2 Struct attributes
+    Vector2 position ;//Vector 2 Struct attributes -> Removed due to GetRandomPos method
+    Texture2D texture;
+
+    Food()
+    { //Constructor
+      Image image = LoadImage("textures/food.png");
+        texture = LoadTextureFromImage(image);
+        UnloadImage(image);
+        position = GenerateRandomPos();
+    }
+    //Destructor
+    ~Food()
+    {
+        UnloadTexture(texture);
+    }
+
+
 
     void Draw()
     {
-        DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkGreen);
+        //DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkGreen);
+        DrawTexture(texture, position.x * cellSize, position.y * cellSize, WHITE);
+    }
+
+    Vector2 GenerateRandomPos() //Random position for food
+    {
+        float x = GetRandomValue(0, cellCount - 1);
+        float y = GetRandomValue(0, cellCount - 1);
+        return Vector2{x, y};
     }
 
 };
