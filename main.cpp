e5f1618348b504fd42cabee8047e8f35123bff39
D8M <davidmahon0000@gmail.com>
2025-07-05 12:06:46 +0100

    Added Food class and draw logic

--- main.cpp
+++ main.cpp
@@ -2,12 +2,26 @@
 #include <iostream>
 
 using namespace std;
-Color green = {173,204,96,255 };
-Color darkGreen = {43, 51, 255 };
 
-//Grid of 750 by 750 pixels ( 25 * 30)
+Color green = {173,204,96,255 };
+Color darkGreen = {43, 51,24, 255 };
+Color red = {255, 0, 0, 255};
+
+//Grid of 750 by 750 pixels (25 * 30)
 int cellSize = 30;
 int cellCount = 25;
+
+class Food {
+
+public:
+    Vector2 position = {5, 6};//Vector 2 Struct attributes
+
+    void Draw()
+    {
+        DrawRectangle(position.x * cellSize, position.y * cellSize, cellSize, cellSize, darkGreen);
+    }
+
+};
 
 int main() {
     int screen_width = 750;
@@ -16,6 +30,8 @@ int main() {
     InitWindow(cellSize * cellCount, cellSize * cellCount, "Retro Shnake");
     SetTargetFPS(60);
 
+    //Create Objects
+    Food food = Food();
 
 
 
@@ -25,7 +41,7 @@ int main() {
         BeginDrawing();
         //Drawing
         ClearBackground(green);
-
+        food.Draw();
 
 
         EndDrawing();
